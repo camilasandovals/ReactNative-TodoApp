6269c222afd019a15040f33170d8b46364880255
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-07-11 12:09:02 -0400

    added header and list

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react";
-import { NativeBaseProvider, Box, Text } from "native-base";
+import { useState, useEffect } from "react";
+import { NativeBaseProvider, Box } from "native-base";
+import { auth } from "./app/fbConfig";
 import Login from "./app/Login";
 import TodoList from "./app/TodoList";
-import { auth } from "./app/fbConfig";
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState()
+  const [todos, setTodos] = useState()
 
   useEffect(() => {
     const _user = auth.currentUser;
     setUser(_user);
-  }, [auth]);
+  }, [])
 
   return (
     <NativeBaseProvider>
-      <Box>
-        <Text>Todo App</Text>
-        {!user ? <Login /> : <TodoList />}
+      <Box flex={1} bg={"darkBlue.900"} alignItems="center" justifyContent="center">
+        {!user
+          ? <Login setUser={setUser} />
+          : <TodoList user={user} todos={todos} setTodos={setTodos} />
+        }
       </Box>
     </NativeBaseProvider>
   );
